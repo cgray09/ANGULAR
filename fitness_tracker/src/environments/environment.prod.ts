499fab19f22d673898c25fc26b3c8ec0bd0323e6
cgray09 <47266486+cgray09@users.noreply.github.com>
2021-09-13 17:24:28 -0700

    Add files via upload

---
+++ fitness_tracker/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDydNW4k59vZOay0AUUIySGnRXME8y7xPI",
+    authDomain: "fitness-tracker-69553.firebaseapp.com",
+    projectId: "fitness-tracker-69553",
+    storageBucket: "fitness-tracker-69553.appspot.com",
+    messagingSenderId: "893474995854",
+    appId: "1:893474995854:web:ce0b9708b767cd223d53df"  
+  }
+};
---
+++ fitness_tracker/src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDydNW4k59vZOay0AUUIySGnRXME8y7xPI",
+    authDomain: "fitness-tracker-69553.firebaseapp.com",
+    projectId: "fitness-tracker-69553",
+    storageBucket: "fitness-tracker-69553.appspot.com",
+    messagingSenderId: "893474995854",
+    appId: "1:893474995854:web:ce0b9708b767cd223d53df"  
+  }
+};
