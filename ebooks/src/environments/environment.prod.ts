65862e5a851f28e8e28c044001c2a2e7c04e1b1f
cgray09 <47266486+cgray09@users.noreply.github.com>
2021-09-15 23:09:24 -0700

    Add files via upload

---
+++ ebooks/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig:
+  {
+    apiKey: "AIzaSyCRonxuqVDMbVjXfqiN9kWqxJodj5WtSbU",
+    authDomain: "ebooks-d2841.firebaseapp.com",
+    databaseURL: "https://ebooks-d2841-default-rtdb.firebaseio.com",
+    projectId: "ebooks-d2841",
+    storageBucket: "ebooks-d2841.appspot.com",
+    messagingSenderId: "1070132107590",
+    appId: "1:1070132107590:web:5b99c946c0b2633792ad47"
+  }
+};
---
+++ ebooks/src/environments/environment.ts
@@ -0,0 +1,35 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig:
+  {
+    apiKey: "AIzaSyCRonxuqVDMbVjXfqiN9kWqxJodj5WtSbU",
+    authDomain: "ebooks-d2841.firebaseapp.com",
+    databaseURL: "https://ebooks-d2841-default-rtdb.firebaseio.com",
+    projectId: "ebooks-d2841",
+    storageBucket: "ebooks-d2841.appspot.com",
+    messagingSenderId: "1070132107590",
+    appId: "1:1070132107590:web:5b99c946c0b2633792ad47"
+
+    // apiKey: "AIzaSyD8ltSyoqsBp1ZgdDJMYakUV8jeEUW29JA",
+    // authDomain: "project-firebase-university.firebaseapp.com",
+    // databaseURL: "https://project-firebase-university.firebaseio.com",
+    // projectId: "project-firebase-university",
+    // storageBucket: "project-firebase-university.appspot.com",
+    // messagingSenderId: "705297937497",
+    // appId: "1:705297937497:web:513ca8cff7b4285d69ad48",
+    // measurementId: "G-2FEZJH9PBZ"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
