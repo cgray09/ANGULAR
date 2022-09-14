0727f78b3b75e4dd2555fee64f24bfc8e065ed42
cgray09 <47266486+cgray09@users.noreply.github.com>
2022-09-14 06:59:04 -0700

    Update recipes-routing.module.ts

--- recipes/src/app/recipes/recipes-routing.module.ts
+++ recipes/src/app/recipes/recipes-routing.module.ts
@@ -16,6 +16,7 @@ const routes: Routes = [
     children: [
       { path: '', component: RecipeStartComponent },
       { path: 'new', component: RecipeEditComponent },
+      // routes with paramaters must come last
       {
         path: ':id',
         component: RecipeDetailComponent,
