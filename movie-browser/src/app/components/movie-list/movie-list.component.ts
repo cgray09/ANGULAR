16c7c26f5d74c62536f66ff44f018d653c588c52
cgray09 <47266486+cgray09@users.noreply.github.com>
2024-10-10 22:13:47 -0700

    Add files via upload

--- movie-browser/src/app/components/movie-list/movie-list.component.ts
+++ movie-browser/src/app/components/movie-list/movie-list.component.ts
@@ -1,30 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { MoviesService } from 'src/app/services/movies.service';
+import { MoviesService } from '../../services/movies.service';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movie-list',
-  templateUrl: './movie-list.component.html',
-  styleUrls: ['./movie-list.component.css']
+  templateUrl: './movie-list.component.html'
 })
 export class MovieListComponent implements OnInit {
 
-  movies: any[];
+  movies!: any[];
 
   constructor(private service: MoviesService,
     private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
-
     this.activatedRoute.queryParams.subscribe(
       qparams=>{
-        let q = qparams['q'];
+        let q  = qparams['q'] ? qparams['q'] : "X-Men";
+        console.log("qparams: ", qparams)
         this.service.searchMovies(q)
           .subscribe(resp=>this.movies=resp.Search)
       }
-    )
-
-    ;
+    );
   }
-
 }
